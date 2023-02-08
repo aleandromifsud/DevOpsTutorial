a74c366af0d8ca83adfd11ea60c4e8c3268d5437
Ale <mifsud.aleandro@gmail.com>
2023-02-08 19:44:30 +0100

    fixed ansible and added mongoDB, need to fix communication between containers

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -1,40 +1,31 @@
+import pymongo
 import os
-import tempfile
-from functools import reduce
-
-from tinydb import TinyDB, Query
-
-db_dir_path = tempfile.gettempdir()
-db_file_path = os.path.join(db_dir_path, "students.json")
-student_db = TinyDB(db_file_path)
+url = os.environ['MONGO_URI'] or "mongodb://localhost:27017/"
+dbClient = pymongo.MongoClient(url)
+student_db = dbClient["students"]
+studentCol = student_db["student"]
 
 
 def add(student=None):
-    queries = []
-    query = Query()
-    queries.append(query.first_name == student.first_name)
-    queries.append(query.last_name == student.last_name)
-    query = reduce(lambda a, b: a & b, queries)
-    res = student_db.search(query)
+    res = studentCol.find_one({"student_id": student.student_id})
     if res:
         return 'already exists', 409
 
-    doc_id = student_db.insert(student.to_dict())
-    student.student_id = doc_id
-    return student.student_id
+    studentCol.insert_one(student.to_dict())
+    student_obj = get_by_id(student.student_id)
+    return student_obj['student_id']
+
 
 def get_by_id(student_id=None, subject=None):
-    student = student_db.get(doc_id=int(student_id))
+    print(url)
+    student = studentCol.find_one(dict(student_id=int(student_id)))
     if not student:
         return 'not found', 404
-    student['student_id'] = student_id
-    print(student)
+    student['_id'] = str(student['_id'])
     return student
 
 
 def delete(student_id=None):
-    student = student_db.get(doc_id=int(student_id))
-    if not student:
-        return 'not found', 404
-    student_db.remove(doc_ids=[int(student_id)])
+    student = get_by_id(student_id)
+    studentCol.delete_one({"student_id": student['student_id']})
     return student_id
