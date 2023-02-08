178836fc3828c189ea92673b5e481816ed7516f8
Ale <mifsud.aleandro@gmail.com>
2023-02-08 20:04:45 +0100

    added kubernetes configs, having issues with service pod not coming to life

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -10,14 +10,12 @@ def add(student=None):
     res = studentCol.find_one({"student_id": student.student_id})
     if res:
         return 'already exists', 409
-
     studentCol.insert_one(student.to_dict())
     student_obj = get_by_id(student.student_id)
     return student_obj['student_id']
 
 
 def get_by_id(student_id=None, subject=None):
-    print(url)
     student = studentCol.find_one(dict(student_id=int(student_id)))
     if not student:
         return 'not found', 404
